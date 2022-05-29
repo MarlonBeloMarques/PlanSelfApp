3fda097fe37f445e35d0bec427e5aa1a0b1af74a
Marlon Marques <marlon.belohd@gmail.com>
2022-05-29 16:53:28 -0300

    test(ui/welcome): should press the button with success

--- src/presentation/screens/welcome/welcome.tsx
+++ src/presentation/screens/welcome/welcome.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
-const Welcome: React.FC = () => {
+type Props = {
+  buttonAction: () => void;
+};
+
+const Welcome: React.FC<Props> = ({ buttonAction }) => {
   return (
     <View style={{ flex: 1 }}>
       <Text>PLANSELF</Text>
       <Text testID="subtitle_id">
         You need to plan for have success in your life
       </Text>
-      <TouchableOpacity testID="button_id">
+      <TouchableOpacity testID="button_id" onPress={buttonAction}>
         <Text testID="button_label_id">I WANT TO PLAN</Text>
       </TouchableOpacity>
     </View>
--- tests/ui/screens/welcome.spec.tsx
+++ tests/ui/screens/welcome.spec.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import WelcomeView from '../../../src/presentation/screens/welcome/welcome';
 
 describe('UI: Welcome', () => {
   test('should show title with success', () => {
-    const { getByText } = render(<WelcomeView />);
+    const { getByText } = render(<WelcomeView buttonAction={() => {}} />);
 
     const title = getByText('PLANSELF');
     expect(title).toBeTruthy();
   });
 
   test('should show subtitle with success', () => {
-    const { getByTestId } = render(<WelcomeView />);
+    const { getByTestId } = render(<WelcomeView buttonAction={() => {}} />);
 
     const subtitle = getByTestId('subtitle_id');
     expect(subtitle).toBeTruthy();
   });
 
   test('should show button with label', () => {
-    const { getByTestId } = render(<WelcomeView />);
+    const { getByTestId } = render(<WelcomeView buttonAction={() => {}} />);
 
     const button = getByTestId('button_id');
     const label = getByTestId('button_label_id');
@@ -26,5 +26,18 @@ describe('UI: Welcome', () => {
     expect(button).toBeTruthy();
     expect(label).toBeTruthy();
     expect(label.props.children).toBeTruthy();
+  });
+
+  test('should press the button with success', () => {
+    const buttonAction = jest.fn();
+    const { getByTestId } = render(<WelcomeView buttonAction={buttonAction} />);
+
+    const button = getByTestId('button_id');
+
+    expect(button).toBeTruthy();
+
+    fireEvent.press(button);
+
+    expect(buttonAction).toHaveBeenCalledTimes(1);
   });
 });

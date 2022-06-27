baa8681c4d3e4c36889ab4786ee6ed159dda1318
Marlon Marques <marlon.belohd@gmail.com>
2022-06-27 08:32:45 -0300

    test(data/get-status-add-plan-remote-config): refactor tests using makeSut

--- tests/data/useCases/getStatusAddPlanRemoteConfig.spec.ts
+++ tests/data/useCases/getStatusAddPlanRemoteConfig.spec.ts
@@ -4,23 +4,20 @@ import { RemoteConfigSpy } from '../remoteConfig/remoteConfigSpy';
 
 describe('Data: GetStatusAddPlanRemoteConfig', () => {
   test('should get with GetRemoteConfig call correct param', async () => {
-    const remoteConfigSpy = new RemoteConfigSpy();
-    const sut = new getStatusAddPlanRemoteConfig(remoteConfigSpy);
+    const { sut, remoteConfigSpy } = makeSut();
     await sut.get('addPlan');
     expect(remoteConfigSpy.param).toEqual('addPlan');
   });
 
   test('should get with GetRemoteConfig returning successfully', async () => {
-    const remoteConfigSpy = new RemoteConfigSpy();
-    const sut = new getStatusAddPlanRemoteConfig(remoteConfigSpy);
+    const { sut, remoteConfigSpy } = makeSut();
     remoteConfigSpy.completeWithStatus(true);
     const result = await sut.get('addPlan');
     expect(result).toBe(true);
   });
 
   test('should return exception if not get with success via GetRemoteConfig', async () => {
-    const remoteConfigSpy = new RemoteConfigSpy();
-    const sut = new getStatusAddPlanRemoteConfig(remoteConfigSpy);
+    const { sut, remoteConfigSpy } = makeSut();
     remoteConfigSpy.completeWithStatus(true);
     remoteConfigSpy.completeWithUnexpectedError();
 
@@ -32,3 +29,10 @@ describe('Data: GetStatusAddPlanRemoteConfig', () => {
     }
   });
 });
+
+const makeSut = () => {
+  const remoteConfigSpy = new RemoteConfigSpy();
+  const sut = new getStatusAddPlanRemoteConfig(remoteConfigSpy);
+
+  return { sut, remoteConfigSpy };
+};

1bda1f2454e0c52189987730edd4042bc3b1b7c2
Marlon Marques <marlon.belohd@gmail.com>
2022-07-09 10:26:35 -0300

    test(ui/activity): should show list of my plans with success

--- src/presentation/screens/activity/activity.tsx
+++ src/presentation/screens/activity/activity.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import { GetMyPlans } from '~/domain/useCases';
 
-const Activity: React.FC = () => {
+type Props = {
+  myPlans: GetMyPlans.List;
+};
+
+const Activity: React.FC<Props> = ({ myPlans }) => {
   return (
     <View>
       <View testID="header_container_id">
         <Text testID="header_title_id">My Plans</Text>
       </View>
+      <FlatList
+        testID="list_my_plans_id"
+        data={myPlans}
+        renderItem={() => <View />}
+      />
     </View>
   );
 };
--- tests/ui/screens/activity.spec.tsx
+++ tests/ui/screens/activity.spec.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { GetMyPlans } from '~/domain/useCases';
 import ActivityView from '../../../src/presentation/screens/activity/activity';
 
 describe('UI: Activity', () => {
   test('should show header container with success', () => {
-    const { getByTestId } = render(<ActivityView />);
+    const { getByTestId } = render(
+      <ActivityView myPlans={[] as GetMyPlans.List} />,
+    );
     const headerContainer = getByTestId('header_container_id');
     expect(headerContainer).toBeTruthy();
   });
 
   test('should show header title with success', () => {
-    const { getByTestId } = render(<ActivityView />);
+    const { getByTestId } = render(
+      <ActivityView myPlans={[] as GetMyPlans.List} />,
+    );
     const headerTitle = getByTestId('header_title_id');
     expect(headerTitle).toBeTruthy();
+  });
+
+  test('should show list of my Plans with success', () => {
+    const { getByTestId } = render(
+      <ActivityView myPlans={[] as GetMyPlans.List} />,
+    );
+    const listMyPlans = getByTestId('list_my_plans_id');
+    expect(listMyPlans).toBeTruthy();
   });
 });

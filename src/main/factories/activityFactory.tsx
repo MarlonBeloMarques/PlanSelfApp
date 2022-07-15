bc478a7bfde9b01471f227f33682376e2788a6a5
Marlon Marques <marlon.belohd@gmail.com>
2022-07-15 18:44:51 -0300

    feat(main/activityfactory): add firebase config to ios and add dependencies via props

--- src/main/factories/activityFactory.tsx
+++ src/main/factories/activityFactory.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { RouteProp } from '@react-navigation/native';
+import firebaseRemoteConfig from '@react-native-firebase/remote-config';
+import firebaseDatabase from '@react-native-firebase/database';
 import { Activity } from '~/presentation/screens';
 import { Routes } from '~/main/navigation';
+import { FirebaseAdapter } from '~/infra';
+import {
+  GetMyPlansDatabase,
+  GetStatusAddPlanRemoteConfig,
+} from '~/data/useCases';
 
 type Props = {
   route: RouteProp<StackParams, Routes>;
@@ -9,7 +16,23 @@ type Props = {
 };
 
 const ActivityFactory: React.FC<Props> = () => {
-  return <Activity />;
+  const path = FirebaseAdapter.convertToPath({
+    user: 'user',
+    myPlans: 'myPlans',
+  });
+
+  const remoteConfig = firebaseRemoteConfig();
+  const database = firebaseDatabase();
+  const databaseReference = database.ref(path);
+
+  const firebase = new FirebaseAdapter(remoteConfig, databaseReference);
+
+  const getMyPlans = new GetMyPlansDatabase(firebase);
+  const getStatusAddPlan = new GetStatusAddPlanRemoteConfig(firebase);
+
+  return (
+    <Activity getMyPlans={getMyPlans} getStatusAddPlan={getStatusAddPlan} />
+  );
 };
 
 export default ActivityFactory;
